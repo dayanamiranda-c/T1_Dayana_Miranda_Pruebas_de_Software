9979923cf8b7fe79b37452436cd07d6335b82116
Dayana Miranda <dayanamdc@gmail.com>
2026-03-30 17:46:53 -0500

    Pregunta 2: Lógica de Código Devolución implementada - Fase Verde

--- src/main/java/DevolucionService.java
+++ src/main/java/DevolucionService.java
@@ -1,7 +1,13 @@
 import java.time.LocalDate;
 
 public class DevolucionService {
-    public String registrar(String codDev, String codProd, String cliente, String motivo, LocalDate fCompra, LocalDate fDev) {
+    /*public String registrar(String codDev, String codProd, String cliente, String motivo, LocalDate fCompra, LocalDate fDev) {
         return ""; // Retornamos vacío para que el test falle
+    }*/
+    public String registrar(String codDev, String codProd, String cliente, String motivo, LocalDate fCompra, LocalDate fDev) {
+        if (!codDev.matches("^D\\d{4}$")) {
+            return "Ingrese un código de devolución válido";
+        }
+        return "La devolución ha sido registrada correctamente";
     }
 }
